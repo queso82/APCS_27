1afb2cb0e3cadfbb7601f1ced7abfb63afaafca2
queso82 <janoa6533@gmail.com>
2026-09-21 21:36:15 +0000

    update

--- 102_Exercises/10_RandomRange/base_code/starter.java
+++ 102_Exercises/10_RandomRange/base_code/starter.java
@@ -10,6 +10,34 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
-	}
+		System.out.println("Enter 2 numbers to create a range for your random number"); 
+		Scanner bru = new Scanner(System.in);
+		
+		System.out.print("Please enter an integer: ");
+		int y = bru.nextInt();
+		Scanner bar = new Scanner(System.in);
+		
+		System.out.print("Please enter another integer (bigger than the first): ");
+		int z = bar.nextInt();
+		System.out.print("Your range is ");
+		System.out.print(y);
+		System.out.print(" to ");
+		System.out.println(z);
+        System.out.println("Here are 5 numbers generated in that range.");
+		int qq = (int)(Math.random() * (z-y)+y);
+
+		System.out.print(qq);
+		System.out.print(", ");
+		int we = (int)(Math.random() * (z-y)+y);
+		int sq = (int)(Math.random() * (z-y)+y);
+		int eq = (int)(Math.random() * (z-y)+y);
+		int tq = (int)(Math.random() * (z-y)+y);
+		System.out.print(we);
+		System.out.print(", ");
+		System.out.print(sq);
+		System.out.print(", ");
+		System.out.print(eq);
+		System.out.print(", ");
+		System.out.println(tq);
+	}   
 }
